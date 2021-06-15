0ea01949a92ec6f59e22fd9ac511317eb64a9934
simphotonics <git@simphotonics.com>
2021-06-15 22:10:38 +0100

    Added export statement.

--- lib/stride.dart
+++ lib/stride.dart
@@ -1,8 +1,5 @@
-/// Generalized iterator and iterable. Enables iterating a Dart list with
+/// Enables iterating a Dart lists and iterables with
 /// a custom step size and start index.
 library stride;
 
-export 'src/fast_stride_iterable.dart';
-export 'src/fast_stride_iterator.dart';
-export 'src/stride_iterable.dart';
-export 'src/stride_iterator.dart';
+export 'src/stride_extension.dart';

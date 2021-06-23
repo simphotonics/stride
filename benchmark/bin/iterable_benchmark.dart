8a503e65c473211cb4fd2a75a2c7a9abbb713f59
simphotonics <git@simphotonics.com>
2021-06-23 15:50:54 +0100

    Increased benchmark run time.

--- benchmark/bin/iterable_benchmark.dart
+++ benchmark/bin/iterable_benchmark.dart
@@ -3,25 +3,7 @@ import 'dart:typed_data';
 import 'package:benchmark/benchmark.dart';
 import 'package:stride/stride.dart';
 
-extension StrideII<E> on Iterable<E> {
-  Iterable<E> strideII(int stepSize, int startIndex) {
-    var counter = 0;
-    return this.skip(startIndex).skipWhile((_) {
-      if (counter == 0) {
-        ++counter;
-        return true;
-      } else if (counter == stepSize - 1) {
-        counter = 0;
-        return false;
-      } else {
-        ++counter;
-        return false;
-      }
-    });
-  }
-}
-
-final nRows = 100000;
+final nRows = 1000000;
 final nCols = 10;
 final stepSize = nCols;
 final startIndex = 4;
@@ -33,10 +15,9 @@ final array2d = List<List<double>>.generate(
 final list = List<double>.generate(
   nRows * nCols,
   (i) => i.toDouble(),
-  growable: false,
+  growable: true,
 );
 final typedList = Float64List.fromList(list);
-
 final typedListIt = typedList.stride(stepSize, startIndex, false);
 final listFastIt = list.stride(stepSize, startIndex, false);
 final listIt = list.stride(stepSize, startIndex);
@@ -44,29 +25,28 @@ final listIt = list.stride(stepSize, startIndex);
 var tmp = 0.0;
 
 void main() {
-  setUp(() {});
 
-  group('column_49:', () {
+  group('column_4:', () {
     benchmark('array2d[i][4]', () {
       // ignore: unused_local_variable
-      var column_49 = [
+      var column_4 = [
         for (var i = 0; i < array2d.length; i++) tmp = array2d[i][startIndex]
       ];
-    }, duration: Duration(milliseconds: 600));
+    }, duration: Duration(milliseconds: 6000));
     benchmark('list iterator checked', () {
       for (var element in listIt) {
         tmp = element;
       }
-    }, duration: Duration(milliseconds: 600));
+    }, duration: Duration(milliseconds: 6000));
     benchmark('list iterator', () {
       for (var element in listFastIt) {
         tmp = element;
       }
-    }, duration: Duration(milliseconds: 600));
+    }, duration: Duration(milliseconds: 6000));
     benchmark('typed list iterator', () {
       for (var element in typedListIt) {
         tmp = element;
       }
-    }, duration: Duration(milliseconds: 600));
+    }, duration: Duration(milliseconds: 6000));
   });
 }
